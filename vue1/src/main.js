821c8da4bac4d945f26e8bd87f72e3f3f14520b7
LeewayTang <1009651761@qq.com>
2021-04-29 18:09:51 +0800

    add axios

--- vue1/src/main.js
+++ vue1/src/main.js
@@ -9,11 +9,12 @@ import {parseTime} from './utils'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 import vcolorpicker from 'vcolorpicker'
+import axios from 'axios'
 
 Vue.use(vcolorpicker)
 Vue.use(ViewUI);
 Vue.config.productionTip = false
-
+Vue.prototype.$axios= axios
 Vue.config.productionTip = false
 Vue.filter('parseTime', (v) => parseTime(v,'{y}-{m}-{d}'))
 new Vue({
