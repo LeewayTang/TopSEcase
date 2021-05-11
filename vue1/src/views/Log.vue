15a79548757ffda8c4dd0291380ea2e602d44c57
song <songyx2001@126.com>
2021-05-12 00:21:12 +0800

    update 右边栏

--- vue1/src/views/Log.vue
+++ vue1/src/views/Log.vue
@@ -5,11 +5,15 @@
   <log-page-main></log-page-main>
   </div>
   <div class="right">
-    <div class="header-block">
+    <div class="square-block">
       <div class="header">
       <router-link to="/personalCenter">
         <img src="../assets/images/助教也是人.png" alt="头像" class="circular">
         <div class="user-name">用户名</div>
+        <div class="follow-and-followed">
+          <div class="follow">99 关注</div> |
+          <div class="followed">99 被关注</div>
+        </div>
       </router-link>
       </div>
     </div>
@@ -38,26 +42,41 @@ name: "Log",
   display: flex;
 }
 .main-container {
-  width: 1200px;
+  width: 1000px;
 }
 .right {
-  margin-left: 100px;
+  margin-left: 20px;
   flex-grow: 1;
 }
 .circular {
-  width: 80px;
-  height: 80px;
+  margin-left: 20px;
+  margin-top: 155px;
+  width: 85px;
+  height: 85px;
   border-radius: 50%;
   border: 3px solid rgba(255,255,255,0.4);
 }
-.header-block {
+.square-block {
+  width: 400px;
+  height: 400px;
+  background: linear-gradient(to bottom, grey 0%, grey 50%,whitesmoke 50%,whitesmoke 100%);
   margin-bottom: 50px;
 }
 .user-name {
+  margin-left: 20px;
   font-weight: bolder;
   font-size: 30px;
   color: #0f0f0f;
-  margin-top: 20px;
+  margin-top: 10px;
 }
-
+.follow-and-followed {
+  margin-left: 20px;
+  margin-top: 10px;
+}
+.follow {
+  display: inline-block;
+}
+.followed {
+  display: inline-block;
+}
 </style>
