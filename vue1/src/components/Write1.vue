b9c9ce8c00cbe24166546d9f0fccbd392f7f44d1
cynic <songyx2001@126.com>
2021-05-18 17:27:31 +0800

    补全创作笔记页面

--- vue1/src/components/Write1.vue
+++ vue1/src/components/Write1.vue
@@ -1,6 +1,35 @@
 <template>
-  <div id="editor">
-    <mavon-editor style="height: 100%"></mavon-editor>
+  <div id="editor-wrap">
+    <div id="edit-tag">
+      <el-tag
+          :key="tag"
+          v-for="tag in dynamicTags"
+          closable
+          :disable-transitions="false"
+          @close="handleClose(tag)">
+        {{tag}}
+      </el-tag>
+      <el-input
+          class="input-new-tag"
+          v-if="inputVisible"
+          v-model="inputValue"
+          ref="saveTagInput"
+          size="small"
+          @keyup.enter.native="handleInputConfirm"
+          @blur="handleInputConfirm"
+      >
+      </el-input>
+      <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+    </div>
+    <div id="editor">
+      <mavon-editor style="height: 100%"></mavon-editor>
+    </div>
+    <el-button type="primary" @click="saveTmp">
+      保存草稿
+    </el-button>
+    <el-button type="success" @click="submit">
+      提交笔记
+    </el-button>
   </div>
 </template>
 <script>
@@ -12,16 +41,70 @@ export default {
   components: {
     mavonEditor
     // or 'mavon-editor': mavonEditor
+  },
+  data() {
+    return {
+      dynamicTags: ['标签一', '标签二', '标签三'],
+      inputVisible: false,
+      inputValue: ''
+    }
+  },
+  methods: {
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+
+    handleInputConfirm() {
+      let inputValue = this.inputValue;
+      if (inputValue) {
+        this.dynamicTags.push(inputValue);
+      }
+      this.inputVisible = false;
+      this.inputValue = '';
+    },
+
+    submit() {
+      // 这咋把markdown编辑器里的内容传出去啊，俺也不会，俺也不敢问
+    },
+    saveTmp() {
+      // 同样不会
+    }
   }
 }
 </script>
 <style>
-#editor {
-  margin-top: 250px;
+#editor-wrap {
+  margin-top: 100px;
   margin-right: auto;
   margin-left: auto;
   width: 80%;
-  height: 900px;
+  height: 1200px;
   /*z-index:1 !important;*/
 }
+#editor {
+  height: 50%;
+}
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+}
+
 </style>
